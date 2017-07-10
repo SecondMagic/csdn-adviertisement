8b9a28df58cbb91eb32911f4d82b9c40bd7c0cd0
SecondMagic <401698719@qq.com>
2017-07-10 15:33:02 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+if(document.getElementById("nav_show_top_stop")!=null){
+	document.getElementById("nav_show_top_stop").style.display='none';
+}
+if(document.getElementById('layerd')!=null){
+	document.getElementById('layerd').style.display='none'
+}
