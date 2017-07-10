490ed470480d8f2ef95d67b9b24c7703d3201aef
SecondMagic <401698719@qq.com>
2017-07-10 15:34:26 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ if(document.getElementById("nav_show_top_stop")!=null){
 	document.getElementById("nav_show_top_stop").style.display='none';
 }
 if(document.getElementById('layerd')!=null){
-	document.getElementById('layerd').style.display='none'
+	document.getElementById('layerd').style.display='none';
 }
