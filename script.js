92c0e10524adda5039596c100e293ccbf30c9365
SecondMagic <401698719@qq.com>
2018-04-08 14:11:48 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2,5 +2,20 @@ if(document.getElementById("nav_show_top_stop")!=null){
 	document.getElementById("nav_show_top_stop").style.display='none';
 }
 if(document.getElementById('layerd')!=null){
-	document.getElementById('layerd').style.display='none';
+	document.getElementById('layerd').style.display='none'
+}
+if(document.getElementsByClassName("extension_other csdn-tracking-statistics tracking-click")[0]!=null){
+	document.getElementsByClassName("extension_other csdn-tracking-statistics tracking-click")[0].style.display='none'
+}
+if(document.getElementsByClassName("fixRight")[0]!=null){
+	document.getElementsByClassName("fixRight")[0].children[0].style.display='none'
+}
+if(document.getElementById('homepageArticles')!=null){
+	document.getElementById('homepageArticles').style.display='none'
+}
+if(document.getElementsByClassName("extension_other csdn-tracking-statistics")[0]!=null){
+	document.getElementsByClassName("extension_other csdn-tracking-statistics")[0].style.display='none'
+}
+if(document.getElementsByClassName("fixRight")[0]!=null){
+	document.getElementsByClassName("fixRight")[0].style.display='none'
 }
